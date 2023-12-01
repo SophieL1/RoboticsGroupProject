8bc749a9f3255686e9e01515ffcbf1178b801955
Sophie L <sophie.lequeu@student.uclouvain.be>
2023-12-01 17:13:16 +0100

    Added fct to order verts of obstacles

--- processObstacles.py
+++ processObstacles.py
@@ -5,8 +5,33 @@ Created on Fri Nov 17 17:22:56 2023
 @author: Lenovo
 """
 import numpy as np
+import math
 from shapely.geometry import Polygon
 
+def calculate_centroid(vertices):
+    centroid_x = sum(x for x, _ in vertices) / len(vertices)
+    centroid_y = sum(y for _, y in vertices) / len(vertices)
+    return centroid_x, centroid_y
+
+def sort_obstacle_vertices(obstacle_vertices):
+    """
+    args : list of list of lists of coordinates of obstacles' vertices (in random order). Obstacles are supposed convex.
+    return : list of list of lists of coordinates of obstacles' vertices, 
+                ordered counterclockwise.
+    """
+    sorted_obstacles = []
+    
+    for obstacle in obstacle_vertices:
+        centroid = calculate_centroid(obstacle)
+        # Calculate angles of vertices with respect to centroid
+        angles = [math.atan2(vertex[1] - centroid[1], vertex[0] - centroid[0]) for vertex in obstacle]
+        # Combine vertices and angles into a list of tuples
+        vertices_with_angles = list(zip(obstacle, angles))
+        # Sort vertices based on angles in counterclockwise order
+        sorted_vertices = [vertex[0] for vertex in sorted(vertices_with_angles, key=lambda x: x[1])]
+        sorted_obstacles.append(sorted_vertices)
+
+    return sorted_obstacles
 
 def expand_obstacles(obstacle_vertices, border_width):
     expanded_obstacles = []
